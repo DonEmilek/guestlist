8c73376218e3b5cc07bd055a1601d3f201ba9068
DonEmilek <emilszturo123@gmail.com>
2022-11-17 00:01:36 +0100

    prefix firebase local env

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import "firebase/compat/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY_FB,
-  authDomain: process.env.AUTH_DOMAIN_FB,
-  projectId: process.env.PROJECT_ID_FB,
-  storageBucket: process.env.STORAGE_BUCKET_FB,
-  messagingSenderId: process.env.MESSASING_SENDER_ID_FB,
-  appId: process.env.APP_ID_FB,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY_FB,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN_FB,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID_FB,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET_FB,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSASING_SENDER_ID_FB,
+  appId: process.env.NEXT_PUBLIC_APP_ID_FB,
 };
 
 // Initialize Firebase
